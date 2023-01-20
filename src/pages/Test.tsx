f79ef7009b34353fe9c73e298c85fc4c33e7c87d
ehfdl <cmaw@naver.com>
2023-01-20 17:40:26 +0900

    💄 design(Test):
    Test UI 기초 설정
    
    by @ehfdl @kidongg

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,13 +2,83 @@ import React from "react";
 import styled from "styled-components";
 
 const Test = () => {
-  return <Wrap>Test</Wrap>;
+  return (
+    <BackGround>
+      <Wrap>
+        <StatusBar></StatusBar>
+        <Image />
+        <TestContainer></TestContainer>
+        <AnswerContainer>
+          <Answer></Answer>
+          <Answer></Answer>
+        </AnswerContainer>
+      </Wrap>
+    </BackGround>
+  );
 };
 
 export default Test;
 
-const Wrap = styled.div`
+const BackGround = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: gray;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrap = styled.div`
+  width: 70%;
+  height: 100%;
+  background-color: #fcdddd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StatusBar = styled.div`
+  width: 80%;
+  height: 50px;
+  position: relative;
+  top: 50px;
+  border-radius: 20px;
+  background-color: #ff6f6f;
+`;
+
+const Image = styled.div`
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  background-color: white;
+  position: relative;
+  top: 80px;
+`;
+
+const TestContainer = styled.div`
+  width: 75%;
+  height: 200px;
+  background-color: white;
+  position: relative;
+  top: 120px;
+`;
+
+const AnswerContainer = styled.div`
+  width: 75%;
+  height: 350px;
+  background-color: #ff6f6f;
+  position: relative;
+  top: 160px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+const Answer = styled.div`
+  width: 80%;
+  height: 100px;
+  background-color: white;
+  position: relative;
+  border-radius: 30px;
 `;
