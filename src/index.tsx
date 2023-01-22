2305506c564f68da557d914f09d6b8248603d2f9
Jeong <hj08hn@gmail.com>
2023-01-22 20:26:40 +0900

    :bug: fix:(index) console.log
    - console.log를 삭제합니다
    
    by @Hah-nna

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { auth } from "./firebase";
 import { GlobalStyle } from "./Global-style";
-console.log(auth);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
