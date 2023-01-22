5bcbea9e662e9d4b542a3214324e4321c23ee5ba
Jeong <hj08hn@gmail.com>
2023-01-22 20:31:38 +0900

    :zap: feat:(history) kakao share api
    -버튼 컴포넌트에 onClick으로 kakaoShare를 불러와 카카오톡 공유하기 기능을 실행시킵니다
    
    by @Hah-nna

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import kakaoShare from "../kakao";
+import Button from "../UI/CustomButton";
 
 const Result = () => {
   const { state } = useLocation();
-  return <div>{state}</div>;
+  return (
+    <>
+      <div>{state}</div>
+      <Button onClick={kakaoShare}>공유하기</Button>
+    </>
+  );
 };
 
 export default Result;
