4aa3bedbb05a0ec91a62895820c2ea3b028ebe5c
Jeong <hj08hn@gmail.com>
2023-01-23 15:13:02 +0900

    :zap: feat:(historyCard.tsx) add ICardType
    - 인터페이스 ICardType에 여러 요소를 추가합니다
    
    by @Hah-nna

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,31 +1,105 @@
 import React from "react";
 import styled from "styled-components";
 import { Fade } from "react-awesome-reveal";
+import data from "../test.json";
 
-export type IHIstoryType = {
-  children?: string;
-};
+export interface ICardType {
+  children?: string | string[];
+  title?: string;
+  source_text?: string[];
+  source?: string;
+  text?: string;
+}
 
-export const HistoryCard = (props: IHIstoryType) => {
+export const HistoryCard = (props: ICardType) => {
   return (
-    <Fade
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <HistoryCardSection>{props.children}</HistoryCardSection>
-    </Fade>
+    <>
+      <Fade
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <HistoryCardSection>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+          dicta temporibus debitis earum, modi perferendis dolores praesentium
+          natus corrupti, reprehenderit quia cum provident amet, ea esse itaque
+          labore magnam iusto? Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Accusamus dicta temporibus debitis earum, modi
+          perferendis dolores praesentium natus corrupti, reprehenderit quia cum
+          provident amet, ea esse itaque labore magnam iusto? Lorem ipsum dolor
+          sit, amet consectetur adipisicing elit. Accusamus dicta temporibus
+          debitis earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Accusamus dicta temporibus debitis earum, modi perferendis dolores
+          praesentium natus corrupti, reprehenderit quia cum provident amet, ea
+          esse itaque labore magnam iusto? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Accusamus dicta temporibus debitis
+          earum, modi perferendis dolores praesentium natus corrupti,
+          reprehenderit quia cum provident amet, ea esse itaque labore magnam
+          iusto?
+        </HistoryCardSection>
+      </Fade>
+    </>
   );
 };
 
 const HistoryCardSection = styled.div`
   width: 80%;
   height: 80%;
-  background-color: #fff;
   padding: 5rem;
 `;
 
