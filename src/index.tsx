f1788532d952703475ed4fb37a63386d9bd6f9cc
Jeong <hj08hn@gmail.com>
2023-01-23 15:11:34 +0900

    :zap: fix:(index.tsx) delete import auth
    - import한 auth를 삭제합니다
    
    by @Hah-nna

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { auth } from "./firebase";
 import { GlobalStyle } from "./Global-style";
 
 const root = ReactDOM.createRoot(
